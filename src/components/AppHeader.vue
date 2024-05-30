32a48d88022a6d2e4ca569949776edb6826bb5a2
arnaldorosa <159778135+arnaldorosa@users.noreply.github.com>
2024-05-30 16:41:53 +0200

    milestone 0 e 1 con api da sistemare

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,76 @@
+<script>
+import { store } from '../assets/data/store'
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            store,
+            filmUtente: ""
+        }
+    },
+    methods: {
+        filmGen() {
+            const options = {
+            method: 'GET',
+            url: 'https://api.themoviedb.org/3/movie/popular',
+            params: { include_adult: 'false', language: 'en-US', page: '1' },
+            headers: {
+              accept: 'application/json',
+              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiN2ExZTg0ZWI4YzgwNjk0NGNlYmUwNzIzOGExMDRjMiIsInN1YiI6IjY2NTgzOTc4YTlhYWRjMmQzMjJkMjZkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5Tg1YJGVZD4xGXXRzegQzbnt5H0vOO5NqWJ_uDG4JRQ'
+            }
+        };
+        axios
+            .request(options)
+            .then(function (response) {
+            console.log(response.data);
+            })
+            .catch(function (error) {
+            console.error(error);
+        });
+        }
+        
+    }
+}
+</script>
+
+
+<template>
+    <section>
+        <div class="head">
+            <h1>Boolflix</h1>
+            <input v-model="filmUtente" type="text" placeholder="Cosa vuoi vedere?">
+            <button @click="filmGen()">Cerca Film</button>
+        </div>
+    </section>
+</template>
+
+
+<style scoped>
+.head {
+    display: flex;
+    padding: 20px;
+    align-items: center;
+}
+
+.head h1 {
+    font-size: 30px;
+    margin-right: 30px;
+}
+
+.head button {
+    background-color: crimson;
+    color: black;
+    margin-left: 20px;
+    padding: 10px 35px;
+    font-size: 18px;
+    font-weight: 800;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+}
+
+.select {
+    margin-left: 20px;
+    width: 100px;
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,59 @@
+<script>
+import { store } from '../assets/data/store'
+export default {
+    data() {
+        return {
+            store,
+        }
+    }
+}
+</script>
+
+
+<template>
+    <section class="bg-crimson">
+        <div class="white-container">
+            <h3>Found {{ store.films.length }} films</h3>
+            <!-- <div class="cards-container">
+                <div v-for="film in store.films" class="cards">
+                    <img src="" alt="">
+                    <p class="name">{{ film.title }}</p>
+                    <p class="type">{{ film.original_title }}</p>
+                    <p class="type">{{ film.original_language }}</p>
+                    <p class="type">{{ film.vote_average }}</p>
+                </div>
+            </div> -->
+        </div>
+    </section>
+</template>
+
+
+<style scoped>
+.bg-crimson {
+    background-color: crimson;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+}
+
+.white-container {
+    width: 80%;
+    height: 80%;
+    background-color: black;
+}
+
+h3 {
+    background-color: black;
+    color: white;
+    padding: 20px;
+}
+
+/* .cards-container {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+} */
+
+
+</style>
